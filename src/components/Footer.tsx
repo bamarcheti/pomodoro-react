48df175f4a6031108543333e935ab0d093711f0b
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-08 22:44:46 -0400

    [FEAT] Criando arquivo footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="w-full pb-7 pt-10 text-primary text-xs text-center">
+      @ {new Date().getFullYear()} Pomodoro em React feito pela Bárbara MF. Todos os direitos reservados
+    </div>
+  )
+}
+
+export default Footer;
