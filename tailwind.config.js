1dbb38a5f5de057ccaa2b5f139caa0c341d5d490
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-10 18:04:56 -0400

    [FIX] Corrigindo configurações tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3e98c7',
+        secondary: '#858584',
+        default: '#2B2B2B',
+        card: '#3B3B3B',
+        error: '#EF4444'
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
