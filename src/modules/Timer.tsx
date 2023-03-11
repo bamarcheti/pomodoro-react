deb689884e7742f8d2c39a7969a7ff4454ee11f9
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-11 02:10:48 -0400

    [FEAT] Criação da validação dos icones e play e pause

--- src/modules/Timer.tsx
+++ src/modules/Timer.tsx
@@ -1,4 +1,5 @@
 import { ArrowPathIcon, Cog8ToothIcon, PauseIcon, PlayIcon } from '@heroicons/react/24/outline';
+import { useState } from 'react';
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import Footer from '../components/Footer';
@@ -8,6 +9,19 @@ interface ConfigTimerProps {
 }
 
 const Timer = ({ onClick }: ConfigTimerProps) => {
+  const [isPaused, setIsPaused] = useState(false);
+  const [secondsLeft, setSecondsLeft] = useState(0);
+
+  const initTimer = (workMinutes: number) => {
+    setSecondsLeft(workMinutes * 60);
+  };
+
+  // useEffect(() => {
+  //   initTimer();
+
+  //   setTimeout(secondsLeft, 2500);
+  // }, [isPaused]);
+
   return (
     <div className="flex flex-col justify-center pt-10">
       <div className="flex flex-col items-center">
@@ -22,19 +36,27 @@ const Timer = ({ onClick }: ConfigTimerProps) => {
               backgroundColor: '#3e98c7'
             })}
           />
-          <div className="flex flex-row mt-5 justify-end gap-20">
+          <div className="flex flex-row mt-5 justify-center gap-2">
             <div className="flex gap-3">
-              <PlayIcon className="h-6 w-6 text-primary inline-block cursor-pointer" title="Play" />
-              <PauseIcon
-                className="h-6 w-6 text-primary inline-block cursor-pointer"
-                title="Pause"
-              />
-              <ArrowPathIcon
-                className="h-6 w-6 text-primary inline-block cursor-pointer"
-                title="Reset"
-              />
+              {isPaused ? (
+                <PlayIcon
+                  className="h-6 w-6 text-primary inline-block cursor-pointer"
+                  title="Play"
+                />
+              ) : (
+                <div className="flex flex-row">
+                  <PauseIcon
+                    className="h-6 w-6 text-primary inline-block cursor-pointer"
+                    title="Pause"
+                  />
+                  <ArrowPathIcon
+                    className="h-6 w-6 text-primary inline-block cursor-pointer"
+                    title="Reset"
+                  />
+                </div>
+              )}
             </div>
-            <div className="flex justify-end">
+            <div className="flex">
               <Cog8ToothIcon
                 className="h-6 w-6 text-primary inline-block cursor-pointer"
                 title="Settings"
