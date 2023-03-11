401568a8bfd0bba7c698b7b26d261bf651e0713c
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-11 13:03:51 -0400

    [FEAT] Criando componente Header

---
+++ src/components/Main/Header.tsx
@@ -0,0 +1,11 @@
+import SocialMedia from './SocialMedia';
+
+const Header = () => {
+  return (
+    <div className="flex flex-row shadow-md">
+      <SocialMedia />
+    </div>
+  );
+};
+
+export default Header;
