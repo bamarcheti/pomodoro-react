54d1a07c5a1651a1645a9563b1f135a5fe862fe5
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-03-11 13:05:46 -0400

    [FEAT] Adicionando novos componentes do MainLayout

---
+++ src/components/Main/MainLayout.tsx
@@ -0,0 +1,18 @@
+import Footer from '../Footer';
+import Header from './Header';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const MainLayout: React.FC<Props> = ({ children }) => {
+  return (
+    <div className="flex flex-col w-full h-screen">
+      <Header />
+      <div className="grow justify-center py-3 xl:p-12 mt-2 rounded-sm px-14">{children}</div>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
