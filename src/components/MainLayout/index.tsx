a74884d35c2196ca54334b98e3ec7717c036a006
Bárbara Marcheti Fiorin <bfiorin@psgtecnologiaaplicada.com.br>
2023-12-19 11:38:52 -0400

    refactor: Corrigindo arquitetura do MainLayout

--- src/components/MainLayout/index.tsx
+++ src/components/MainLayout/index.tsx
@@ -1,5 +1,5 @@
-import Footer from './Footer';
-import Header from './Header';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 interface Props {
   children: React.ReactNode;
